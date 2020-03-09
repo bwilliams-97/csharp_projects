f413a3ade5e280f8cf2eaae3409e9a1ee9936ca4
Ben Williams <brw32@cam.ac.uk>
2020-03-09 22:16:11 +0000

    ASTVisitor created. Shifted to Utils folder

--- CodeStructureExtractor/Program.cs
+++ CodeStructureExtractor/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
 
 namespace CodeStructureExtractor
 {
@@ -21,6 +23,14 @@ namespace CodeStructureExtractor
             string baseDir = args[0];
             string solutionPath = args[1];
             string outputDir = args[2];
+
+            void ExploreSolution(SyntaxTree syntaxTree, SemanticModel semanticModel)
+            {
+                var callGraph = CallGraphVisitor.ExtractCallGraph(syntaxTree, semanticModel);
+                var ast = ASTVisitor.ExtractAST(syntaxTree, semanticModel);
+            }
+
+            CSharpSolutionBuilder.BuildSolution(solutionPath, ExploreSolution);
         }
     }
 }
